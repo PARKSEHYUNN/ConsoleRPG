35e92d29583550df07662a9dbbf4e2b3dea4f819
AokumoRin <parksehyun2025@gmail.com>
2025-05-15 00:58:56 +0900

    2025-05-14

--- static/script/index.js
+++ static/script/index.js
@@ -1,82 +1,66 @@
 // static/script/index.js
 
 $(document).ready(() => {
-    // 현재 위치
-    let now = "main";
+    // 메인 메뉴 관리
+    mainMenuManager();
+
+    // 로그인 페이지 관리
+    loginPageManager();
+});
+
+// 메인 메뉴 관리
+const mainMenuManager = () => {
+    // 모든 컨테이너 숨기기
+    $(".container").hide();
+
+    // 메인 컨테이너 보이기
+    $("#main-container").show();
 
     // 타이틀
     const TITLE = "   _____                      _      _____  _____   _____ \n  / ____|                    | |    |  __ \\|  __ \\ / ____|\n | |     ___  _ __  ___  ___ | | ___| |__) | |__) | |  __ \n | |    / _ \\| '_ \\/ __|/ _ \\| |/ _ \\  _  /|  ___/| | |_ |\n | |___| (_) | | | \\__ \\ (_) | |  __/ | \\ \\| |    | |__| |\n  \\_____\\___/|_| |_|___/\\___/|_|\\___|_|  \\_\\_|     \\_____|\n";
     $(".main-title").append($("<pre>").addClass("font").text(TITLE));
+    
+    const SUB_TITLE = " __   __          ___          \n \\ \\ / /__ _ _   |   \\ _____ __\n  \\ V / -_) '_|  | |) / -_) V /\n   \\_/\\___|_|(_) |___/\\___|\\_/ \n                               ";
+    $(".main-sub-title").append($("<pre>").addClass("font").text(SUB_TITLE));
+    
 
-    // 메인 버튼
-    $(".main-button").hover(function () {
-        const mainButton = $(".main-button");
+    // 키보드 조작
+    let nowMenuSelect = gameState.main.nowMenuSelect;
 
-        mainButton.map((i, element) => {
-            const TEXT = $(element).attr("data-text");
-
-            $(element).text(TEXT);
-        });
-
-        const TEXT = $(this).attr("data-text");
-
-        $(this).text(`▸ ${TEXT} ◂`);
-
-        gameState.main.menu = $(this).index(".main-button");
-    }, function () {
-        const mainButton = $(".main-button");
-
-        mainButton.map((i, element) => {
-            const TEXT = $(element).attr("data-text");
-
-            $(element).text(TEXT);
-        });
-
-        const TEXT = $(this).attr("data-text");
-
-        $(this).text(TEXT);
-    });
-
-    // 키보드 입력
-    $(document).keydown(function (event) {
-        // 메인 메뉴
-        if(gameState.location === "main") {
-            switch(event.key) {
-                case "ArrowUp":
-                    mainMenuSelect("up");
-                    break;
-
-                case "ArrowDown":
-                    mainMenuSelect("down");
-                    break;
-            }
+    $(document).keydown((event) => {
+        if(event.key === "ArrowUp" && nowMenuSelect !== 0) 
+            menuSelect(-- nowMenuSelect);
+        else if(event.key === "ArrowDown" && nowMenuSelect !== 2)
+            menuSelect(++ nowMenuSelect);
+        else if(event.key === "Enter") {
+            
         }
-    })
-});
-
-// 메인 메뉴 위치 조정
-const mainMenuSelect = (arrow) => {
-    switch(arrow) {
-        case "up":
-            if(gameState.main.menu !== 0) 
-                gameState.main.menu -= 1;
-            break;
-
-        case "down":
-            if(gameState.main.menu !== 2)
-                gameState.main.menu += 1;
-            break;
-    }
-
-    const mainButton = $(".main-button");
-
-    mainButton.map((i, element) => {
-        const TEXT = $(element).attr("data-text");
-
-        $(element).text(TEXT);
     });
 
-    const TEXT2 = $(mainButton[gameState.main.menu]).attr("data-text");
+    // 메인 메뉴 선택 마우스 조작
+    $(".main-button").hover(function () {
+        nowMenuSelect = $(this).index(".main-button");
+        menuSelect(nowMenuSelect);
+    });
+};
 
-    $(mainButton[gameState.main.menu]).text(`▸ ${TEXT2} ◂`);
+// 메인 메뉴 선택
+const menuSelect = (select) => {
+    // 전체 버튼 초기화
+    $(".main-button").map((i, element) => {
+        $(element).removeClass("active");
+        $(element).text($(element).attr("data-text"));
+    });
+
+    // 선택 버튼 활성화
+    $($(".main-button")[select]).addClass("active");
+    $($(".main-button")[select]).text(`▸ ${$($(".main-button")[select]).attr("data-text")} ◂`);
+}
+
+// 로그인 페이지 관리
+const loginPageManager = () => {
+    // 타이틀
+    const TITLE = "  _              _      \n | |   ___  __ _(_)_ _  \n | |__/ _ \\/ _` | | ' \\ \n |____\\___/\\__, |_|_||_|\n           |___/        ";
+    $(".login-title").append($("<pre>").addClass("font").text(TITLE));
+    
 }
--- static/script/state.js
+++ static/script/state.js
@@ -3,6 +3,15 @@
 const gameState = {
     location: "main",
     main: {
-        menu: 0
+        nowMenuSelect: 0
     }
+};
+
+// 페이지 이동
+const movePage = (page) => {
+    // 모든 컨테이너 숨기기
+    $(".container").hide();
+
+    // 보이고 싶은 컨테이너 보이기
+    $(`#${page}-container`).show();
 };
